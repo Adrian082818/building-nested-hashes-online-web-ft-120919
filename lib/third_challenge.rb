934b291f516cd53480fe69cc98f767abf59917ad
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-12 01:56:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,25 +8,20 @@ def third_challenge
         name: "Lady Montague", age: "54"
         },
       :hero => {
-        :name => {"Romeo"}
-        :age => {"15"}
-        :status => {"alive"}
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name => {"Lord Capulet"}
-        :age => {"50"}
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
-        :name => {"Lady Capulet"}
-        :age => {"51"}
+        name: "Lady Capulet" age: "51"
 
         },
       :heroine => {
-        :name => {"Juliet", :age => "15"}
-        :status => {"alive"}
+        name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
